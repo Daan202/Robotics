c5ccdfce10276fcf3aae2191c4d811820595e726
Daan202 <daan.roelofs2@ru.nl>
2026-04-08 16:25:53 +0200

    update

--- StarterFiles/lineMaze.py
+++ StarterFiles/lineMaze.py
@@ -3,6 +3,7 @@ from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 
 client = RemoteAPIClient()
 sim = client.require("sim")
+# sim.loadScene('./StarterFiles/scenes/lineMaze.ttt') # from: https://www.youtube.com/watch?v=18P7azRsJY0
 
 # HANDLES FOR ACTUATORS AND SENSORS
 left_motor = Motor(sim, DeviceNames.MOTOR_LEFT_LINE, Direction.CLOCKWISE)
@@ -62,14 +63,16 @@ def PID_control(kP, kI, kD, error):
 
 
 # Starts coppeliasim simulation if not done already
+simulation_duration = 100
+
 sim.setStepping(True)
 
 sim.startSimulation()
 
 # MAIN CONTROL LOOP
-while (t := sim.getSimulationTime()) < 3:
-    # follow_line()
-    sim.step()
+while (t := sim.getSimulationTime()) < simulation_duration:
+    follow_line()
+    # sim.step()
 sim.stopSimulation()
 
 
