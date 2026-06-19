62128e37245c3e5afa2556179627a077a2292bdd
islam Abdelaziz <Islam.abdelaziz@outlook.com>
2026-06-19 22:59:03 +0200

    cleaned up the wall_e code

--- StarterFiles/wall_e_script.py
+++ StarterFiles/wall_e_script.py
@@ -1,7 +1,3 @@
-
-from math import fabs
-from shutil import move
-
 import numpy as np
 from robots import *
 import time
@@ -24,15 +20,14 @@ front_camera = ImageSensor(sim, DeviceNames.SMALL_IMAGE_SENSOR_OS)
 left_motor = Motor(sim, DeviceNames.MOTOR_LEFT_OS, Direction.CLOCKWISE)
 right_motor = Motor(sim, DeviceNames.MOTOR_RIGHT_OS, Direction.CLOCKWISE)
 
-class Status (Enum):
+class Behaviours (Enum):
 	NEEDCHARGING =1
 	CHARGED  =2
 	SEARCH  =3 
 	HANDLE_CUBE =4
 	COMPRESS =5
 	DELIVERING =6
-	DONE =7
-	AVOID_WALL =8
+	AVOID_WALL =7
 #-----------------------------------------------------------------------------------------------
 # configurations variables
 MAX_SPEED = 6
@@ -43,7 +38,7 @@ SLOW_SPEED = 1
 
 
 MIN_PIXELS = 25
-LOW_BATTERY = 0.80
+LOW_BATTERY = 0.25
 CLEAR_SONAR = 0.21
 
 #-----------------------------------------------------------------------------------------------
@@ -51,7 +46,7 @@ CLEAR_SONAR = 0.21
 last_print_time = 0.0
 drive_timer_started = False
 timer_time = 0
-status = Status.SEARCH   #Status.DONE   
+behaviour = Behaviours.SEARCH   #Status.DONE   
 previous_state = 0
 do_not_interrupt = False
 in_position = False
@@ -64,7 +59,6 @@ compress_step =0
 delivery_step =0
 #-----------------------------------------------------------------------------------------------
 # Basic robot movment Helper functions
-
 
 #limit the motor speed between the max and min
 def limit (value):
@@ -100,14 +94,9 @@ def Move_backword(slow = False):
 # Move towards a target 
 def drive_to (target):
 	# target [x]: is -1 on the left and 0 in the center and +1 on the right 
-	# use its value to give steering correction 
+	# using its value to give steering correction 
 	steering = 1.5 * target ["x"]
 
-	if target ["x"] >= 0 :
-		turn_direction =1
-	else:
-		turn_direction =-1 
-	
 	left = FORWARD_SPEED + steering
 	right = FORWARD_SPEED -steering
 	drive(left , right)
@@ -134,10 +123,6 @@ def drive_time (Move_type ="F",sec=0 ):
 		return True	
 #-----------------------------------------------------------------------------------------------
 #sensors Helper functions
-
-def show_image(image):
-    plt.imshow(image)
-    plt.show()
 
 def read_sensors ():
 	top_camera._update_image() 
@@ -167,9 +152,6 @@ def rgb_parts(image  : np.ndarray):
 def colour_mask(image, colour_name):
 
     red, green, blue = rgb_parts(image)
-   # print ("red", red)
-    #print ("green", green)
-    #print ("blue", blue)
 
 	# Plant cubes
     if colour_name == "green":
@@ -177,17 +159,14 @@ def colour_mask(image, colour_name):
 	
     # Trash cubes
     if colour_name == "brown":
-       # return (red > 70) & (green > 18) & (green < 120) & (blue < 90) & (red > blue + 15)
       return (red > 70) &(red < 210) &(green > 15) &(green < 95) &(blue < 80) & (red > green + 35) & (red > blue + 35) 
 		     
     # Plant container
     if colour_name == "blue":
-       # return (blue > 75) & (blue > red + 20) & (blue > green + 10) 
        return (red < 60) & (green > 70)  & (blue > 100) 
 	
     # Trash container
     if colour_name == "red":
-        #return (red > 85) & (red > green + 30) & (red > blue + 30) & (green < 120)
         return (red > 95) & (red < 160) & (red > blue + 70) & (red > blue + 70) & (green < 30)&(blue < 30)
 
     # Charging area
@@ -202,12 +181,7 @@ def colour_mask(image, colour_name):
     if colour_name == "gray":
         brightness = (red + green + blue) / 3
         return (brightness >180) & (brightness <235) & (abs(red - green) < 20) & (abs(red - blue) < 40) & (abs(green - blue) < 35)
-	   # return ( (red == 137) & 
-			# (green == 137) & 
-			# (blue == 137) )
-			 #(abs(red - green) < 25) & 
-			 #(abs(red - blue) < 45) & 
-			 #(abs(green - blue) < 45))
+
        
         
 # find a colour in the image 	
@@ -277,7 +251,6 @@ def close_to_wall (top_image):
 	
 	# wall is  is 0.6 in the hight of the image 
 	if wall is not None:
-		#print("wall y ",wall["y"])
 		if wall["y"] > 0.6 and not do_not_interrupt:
 			return True
 		return False
@@ -292,9 +265,7 @@ def go_to_charger (Top_image):
 		return
 	if charger["pixels"] >=2000:
 		do_not_interrupt = True
-		#print ("charger pixels",charger["pixels"])
 		if drive_time("F",0.5):
-			#print ("arrive")
 			stop()
 			return True
 	drive_to(charger)
@@ -308,7 +279,6 @@ def search ():
 	if search_step == 1 :
 		if drive_time("F",1.3) :
 			search_step =0
-	#rotate(1)
 
 # Reset search paramter after interupting search
 def reset_serch():
@@ -319,7 +289,6 @@ def reset_serch():
 
 # moving towarde cube 
 def handle_cube(top_image,front_image,cube_type):
-	print("cube_type",cube_type)
 	if cube_type ==None:
 		return
 	if cube_type == "trash":
@@ -330,18 +299,14 @@ def handle_cube(top_image,front_image,cube_type):
 		front_opject = find_colour(front_image,"green")
 	
 	if front_opject is not None:
-		print ("front_opject Pixels",front_opject["pixels"])
 		if front_opject["pixels"] >= 4000 :
 			stop()
 			return True	
 	if cube is not None:
-		print ("cube",cube)
 		drive_to(cube)
 	else:
 		Move_forward()
-	
-		
-
+			
 #compress and handel compressed trash cube 	
 def Compress_trash(front_image,sonar) :
 	global compress_step
@@ -380,8 +345,6 @@ def cube_delivery(top_image,front_image,sonar,cube_type):
 
 	global delivery_step,do_not_interrupt
 
-	print ("delivery_step",delivery_step)
-
 	if cube_type == "trash":
 		container = find_colour(top_image,"red")
 		no_cube = find_colour(front_image,"red")
@@ -398,7 +361,6 @@ def cube_delivery(top_image,front_image,sonar,cube_type):
 			reset_serch()
 			drive_to(container)
 			if container["pixels"] >=2000:
-				#print ("charger pixels",container["pixels"])
 				stop()
 				delivery_step =1
 
@@ -406,7 +368,6 @@ def cube_delivery(top_image,front_image,sonar,cube_type):
 	if delivery_step==1:
 		do_not_interrupt = True
 		Move_forward(True)
-		#print ("sonar", sonar)
 		if no_cube is not None:
 			if  no_cube["pixels"] >=2000: #is None:#sonar >= CLEAR_SONAR or
 				print ("cube droped")
@@ -427,124 +388,78 @@ def cube_delivery(top_image,front_image,sonar,cube_type):
 #-----------------------------------------------------------------------------------------------
 # Wall_e controller 
 def wall_e_controller():
-	global status,cube , cube_type ,in_position,previous_state,do_not_interrupt
+	global behaviour,cube , cube_type ,in_position,previous_state,do_not_interrupt,last_print_time
+
 	sensors = read_sensors()
 	now  = time.time()
 
-	Manual = True
-	if Manual:
-			#print ("manual is active")
-
-		if keyboard.is_pressed('w') :   # move forward
-				Move_forward()			
-		elif keyboard.is_pressed('s'):   # move Backword
-				Move_backword()
-		elif keyboard.is_pressed('d'):   # move to the right
-				rotate(1)
-		elif keyboard.is_pressed('a'):   # move to the left
-				rotate(-1)
-		elif keyboard.is_pressed('c'):   
-				robot.compress()
-				print('compress is active ')
-		elif keyboard.is_pressed('b'):   
-				print('battery level',sensors["battery"])
-
-		else:
-			stop()
-
-
-
-	global last_print_time
-	
+	#print useful information every one sec
 	if  now - last_print_time > 1.0:
-		#print ('battery level =',sensors["battery"])
-		#print ("bumper sensor ",sensors["bumper"])
-		#print(" sonar sensor ",sensors["sonar"])
-		#print ("sensor RGP :", sensors ["top_image_rgp"])
-		#print ( "function RGP :", rgb_parts (sensors ["top_image"]))
-		#print (" colouer mask :", colour_mask(sensors ["top_image"],"green"))
-		#print ("find the colouer :", find_colour(sensors ["top_image"],"green"))
-		#print ( "cube:",cube)
-		#print ("cube type :",cube_type)
+		print (" current active behaviour is :", behaviour)
+		print (" Battery : ", sensors["battery"])
+		print (" current cube type:", cube_type)
 		last_print_time = now
 	#-----------------------------------------------------
-	#steps
-
 	#charging is the highest poriorty status 
-	if sensors["battery"] <  LOW_BATTERY and not status == Status.CHARGED : 
-		if status !=Status.NEEDCHARGING:
+	if sensors["battery"] <  LOW_BATTERY and not behaviour == Behaviours.CHARGED : 
+		if behaviour !=Behaviours.NEEDCHARGING:
 			if drive_time("B",0.7):
-				status = Status.NEEDCHARGING 
+				behaviour = Behaviours.NEEDCHARGING 
 	
-	#Avoid walls interrupt
+	#always active check if the robot near a wall if True start avoiding the wall
 	if close_to_wall(sensors["top_image"]):
-		print ("wall is detected")
-		if status != Status.AVOID_WALL:
-			previous_state = status
-		status = Status.AVOID_WALL
+		if behaviour != Behaviours.AVOID_WALL:
+			previous_state = behaviour
+		behaviour = Behaviours.AVOID_WALL
 	
-
-	match status:
-
-		case Status.AVOID_WALL:
-			print ("avoid wall")
+	match behaviour:
+		case Behaviours.AVOID_WALL:
 			if drive_time("R",1.3):
-				status = previous_state
+				behaviour = previous_state
 
-		case Status.NEEDCHARGING :
+		case Behaviours.NEEDCHARGING :
 			if not in_position :
 				in_position = go_to_charger(sensors["top_image"])
-				print (" CHARGING ")
 
 			if  sensors["battery"] >= 0.95 : 
 				in_position = False
-				status = Status.CHARGED
+				behaviour = Behaviours.CHARGED
 
-		case Status.CHARGED :
-			print ("CHARGED")
+		case Behaviours.CHARGED :
 			if drive_time("B",0.5) :
 				do_not_interrupt = False
-				status = Status.SEARCH
+				behaviour = Behaviours.SEARCH
 
-		case Status.SEARCH :
-			print ("SEARCH")
+		case Behaviours.DELIVERING:
+			if cube_delivery(sensors["top_image"],sensors["front_image"],sensors["sonar"],cube_type):
+				behaviour = Behaviours.SEARCH
+
+		case Behaviours.HANDLE_CUBE:
+			if handle_cube(sensors["top_image"],sensors["front_image"],cube_type):
+				if cube_type == "trash":
+					behaviour = Behaviours.COMPRESS
+
+				if cube_type == "plant":
+					behaviour = Behaviours.DELIVERING
+			
+		case Behaviours.COMPRESS:
+			if Compress_trash(sensors["front_image"],sensors["sonar"]):
+				print ("commpressed")
+				behaviour = Behaviours.DELIVERING
+
+		case Behaviours.SEARCH :
 			search()
 			cube , cube_type = find_cube(sensors["top_image"])
 			if cube is not None:
 				reset_serch()
 				stop()
-				status = Status.HANDLE_CUBE
-
-		case Status.HANDLE_CUBE:
-			print ("HANDLE_CUBE")
-			if handle_cube(sensors["top_image"],sensors["front_image"],cube_type):
-				if cube_type == "trash":
-					status = Status.COMPRESS
-
-				if cube_type == "plant":
-					status = Status.DELIVERING
+				behaviour = Behaviours.HANDLE_CUBE
 			
-		case Status.COMPRESS:
-			print ("COMPRESS")
-			if Compress_trash(sensors["front_image"],sensors["sonar"]):
-				print ("commpressed")
-				status = Status.DELIVERING
-		
-		case Status.DELIVERING:
-			print ("DELIVERING")
-			if cube_delivery(sensors["top_image"],sensors["front_image"],sensors["sonar"],cube_type):
-				status = Status.SEARCH
-
-		case Status.DONE:
-				
-			print ("DONE")
-			
-
+#-------------------------------------------------------------------------------------------------------------
 # MAIN CONTROL LOOP
 def main():
 	# Starts coppeliasim simulation if not done already
 	sim.startSimulation()
-	
 	while True:
 		state = sim.getSimulationState()	
 		#print ('state :',state)
